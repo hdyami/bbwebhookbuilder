bcf5c28245c640037e271c5854e492fe877214bb
hailu dyami <hailu.d@gmail.com>
2018-05-22 14:52:01 -0400

    just relearning syntax

--- builder.py
+++ builder.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import subprocess
 import sys
 import argparse
 import json
@@ -8,6 +9,16 @@ from pprint import pprint
 # Ingest Bitbucket webhook json payload
 for line in sys.stdin:
 	bbdata = json.loads(line)
-	pprint(bbdata["repository"]["name"])
+	# pprint(bbdata["repository"]["name"])
+
+	repo = bbdata['repository']
+
+cd_p = subprocess.Popen('cd', '/mnt/builds/'+str(repo['name']), stdout=PIPE, stderr=PIPE)
+print cd_p.communicate()
+
+gitpull_p = subprocess.Popen('git', 'pull', stdout=PIPE, stderr=PIPE)
+print gitpull_p.communicate()
+
+# pprint(repo['name'], depth=3)
 
 print 0
