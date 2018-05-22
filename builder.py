2562c82e37333972da228fd5da989bba094f0afb
hailu dyami <hailu.d@gail.com>
2018-05-22 12:24:49 -0400

    just a stub to start with

---
+++ builder.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import os
+import sys
+import argparse
+import json
+from pprint import pprint
+
+# Ingest Bitbucket webhook json payload
+for line in sys.stdin:
+	bbdata = json.loads(line)
+	pprint(bbdata["repository"])
+
+print 0
