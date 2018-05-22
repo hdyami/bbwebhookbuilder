33596ce1b20dc55ad17b30d5e75b670ac749ebdc
hailu dyami <hailu.d@gmail.com>
2018-05-22 12:29:43 -0400

    dev/test

--- builder.py
+++ builder.py
@@ -8,6 +8,6 @@ from pprint import pprint
 # Ingest Bitbucket webhook json payload
 for line in sys.stdin:
 	bbdata = json.loads(line)
-	pprint(bbdata["repository"])
+	pprint(bbdata["repository"]["name"])
 
 print 0
