ecaebc1b3df963e2a9199f2b9a7267dc1444a8fb
hailu dyami <hailu.d@gmail.com>
2018-06-23 00:00:50 -0400

    creates db, grants permissions fails gracefully

--- dbcreate.py
+++ dbcreate.py
@@ -11,7 +11,7 @@ from pprint import pprint
 # setup our arguments
 parser = argparse.ArgumentParser(description="Provide a database name, target and password")
 parser.add_argument('dbname', nargs='?', default=sys.stdin, help='Name of the site to build')
-parser.add_argument('-d', nargs='?', help="Destination cluster for the db - dev, prod, stag")
+# parser.add_argument('-d', nargs='?', help="Destination cluster for the db - dev, prod, stag")
 # parser.add_argument('-p', nargs='?', help="Password")
 
 args = parser.parse_args()
@@ -19,9 +19,7 @@ args = parser.parse_args()
 config = {
 	'user': 'qsdbadmin',
 	'host': 'dev.umwebdb.umass.edu',
-	#'database': args.dbname,
 	'port': '3307',
-	#'raise_on_warnings': True,
 	'option_files': '/home/jenk/.my.cnf',
 }
 
@@ -29,21 +27,23 @@ cnx = mysql.connector.connect(**config)
 cursor = cnx.cursor(buffered=False)
 
 def create_database(cursor):
-    try:
-        cursor.execute(
-            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(args.dbname))
-    except mysql.connector.Error as err:
-        print("Failed creating database: {}".format(err))
-        exit(1)
+	try:
+		cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(args.dbname))
+		print("Success creating database {}".format(args.dbname))
+	except mysql.connector.Error as err:
+		print("Failed creating database: {}".format(err))
+	#	exit(1)
+
+def enable_database_access(cursor):
+	query = ("GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, INDEX, ALTER, LOCK TABLES, CREATE TEMPORARY TABLES ON {}.* TO %s@%s IDENTIFIED BY %s WITH MAX_USER_CONNECTIONS 30".format('`cryptozoo_drpl`'))
+	try:
+		cursor.execute(query, ('cryptozoo_drpl', 'd7-%.dev.www.umass.edu', 'zYVfEkTgYawVKMum'))
+		print("Success granting permissions for {}".format(args.dbname))
+	except mysql.connector.Error as err:
+		print("Failed querying: {}".format(err))
+		exit(1)
 
 create_database(cursor)
+enable_database_access(cursor)
 
 cnx.close()
-
-# http://python-for-system-administrators.readthedocs.io/en/latest/ssh.html
-# querywrapper = "mysql -uqsdbadmin --port=3307 -hdev.umwebdb.umass.edu -e 'create database "+args.dbname+";'"
-
-
-#ssh -L 2222:umwebdb...:3307 <user>@<host>
-
-#127.0.0.1 2222
