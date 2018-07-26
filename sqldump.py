cad7612f3eff90651b6b8cce4502c952406cefcc
hailu dyami <hailu.d@gmail.com>
2018-07-26 17:12:24 -0400

    cleanup

--- sqldump.py
+++ sqldump.py
@@ -11,7 +11,7 @@ from pprint import pprint
 from umwebpass import password
 
 # setup our arguments
-parser = argparse.ArgumentParser(description="Provide a database name, target and password")
+parser = argparse.ArgumentParser(description="Provide a database name, host and password. Creates a NAME_drpl.sql file at /var/www/NAME/")
 parser.add_argument('sitename', nargs='?', default=sys.stdin, help='Name of the site to build')
 parser.add_argument('-d', nargs='?', help="Source of the dump")
 
@@ -20,27 +20,15 @@ args = parser.parse_args()
 NAME = args.sitename
 HOST = args.d
 
-drush = subprocess.Popen(["ssh", "jenk@"+HOST, "drush", "@"+NAME+"-dev", "sql-dump > /var/www/"+NAME+"/NAME_drpl.sql", shell=False, stdout=subprocess.PIPE)
+if HOST == 'd7-1.dev.www.umass.edu':
+	SUFFIX = '-dev'
+elif HOST == 'd7-1.stag.www.umass.edu':
+	SUFFIX = '-staging'
+elif HOST == 'd7-1.prod.www.umass.edu':
+	SUFFIX = '-prod'
+
+drush = subprocess.Popen(["ssh", "jenk@"+HOST, "drush", "@"+NAME+SUFFIX, "sql-dump > /var/www/"+NAME+"/"+NAME+"_drpl.sql"], shell=False, stdout=subprocess.PIPE)
 drushresponse = drush.communicate()
 
 params = drushresponse[0]
-#fields = params.strip().split()
-
-#user = fields[1]
-#password = fields[2]
-#database = re.sub('--database=', '', fields[3])
-#host = fields[4]
-#port = fields[5]
-
-#print user
-#print password
-#print database
-#print host
-#print port
-
- mysqldump = subprocess.Popen(["mysqldump", database, host, user, password, ">", "/home/jenk/"+NAME+"_drpl.sql"], shell=False, stdout=subprocess.PIPE)
-#mysqldump = subprocess.list2cmdline(["mysqldump", database, host, user, password, ">", "/home/jenk/"+NAME+"_drpl.sql"])
-
-# mysqldumpresponse = mysqldump.communicate()
-
-print mysqldump
+print params
